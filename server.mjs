0dee6db897465758515ef27c663d1d4b27b73279
Alejandro CR <contacto@alejandrocr.co>
2026-07-10 03:59:09 -0500

    refactor server handler

--- server.mjs
+++ server.mjs
@@ -1,69 +1,82 @@
-import { createServer, IncomingMessage, ServerResponse } from 'node:http'
-import { Readable } from 'node:stream'
-import { pipeline } from 'node:stream/promises'
+import { createServer } from 'node:http'
+import { join } from 'node:path'
+import connect from 'connect'
+import serveStatic from 'serve-static'
+import { toNodeHandler } from 'srvx/node'
 
-/** @typedef {Record<string, (request: Request) => Promise<Response>>} HandlersModule */
+/**
+ * @typedef {
+ *   (request: Request) => Response | Promise<Response>
+ * } Handler
+ */
 
-const routes = new Map([['/health', () => import('./api/health.mjs')]])
+/** @typedef {Record<string, unknown>} HandlersModule */
+/** @typedef {() => Promise<HandlersModule>} ModuleLoader */
+
+/** @type {Map<string, ModuleLoader>} */
+const routes = new Map([
+  ['/health', () => import('./api/health.mjs')],
+  ['/react', () => import('./api/react.mjs')]
+])
 
 const port = 3000
-
 const host = 'localhost'
 
 /**
- * @param {Response} response
- * @param {import('node:http').ServerResponse<IncomingMessage> & { req: IncomingMessage }} serverResponse
+ * @param {Request} request
+ * @returns {Promise<Response>}
  */
-async function send(response, serverResponse) {
-  serverResponse.statusCode = response.status
-  serverResponse.statusMessage = response.statusText
-
-  for (const [name, value] of response.headers) {
-    serverResponse.setHeader(name, value)
-  }
-
-  if (response.body === null) {
-    return serverResponse.end()
-  }
-
-  await pipeline(Readable.fromWeb(response.body), serverResponse)
-
-  serverResponse.end()
-}
-
-createServer(async (request, response) => {
+async function fetchHandler(request) {
   console.info(new Date().toUTCString(), request.method, request.url)
 
-  if (request.method === undefined) {
-    response.statusCode = 400
-    return response.end()
-  }
-
-  const pathname = new URL(request.url, `http://${host}:${port}`).pathname
-
-  const moduleLoader = routes.get(pathname)
-
-  if (moduleLoader === undefined) {
-    response.statusCode = 404
-    return response.end()
-  }
-
-  /** @type {HandlersModule} */
-  const module = await moduleLoader()
-
-  const handler = module[request.method]
-
-  if (typeof handler !== 'function') {
-    response.statusCode = 405
-    response.setHeader('Allow', Object.keys(module).join(', '))
-    return response.end()
-  }
-
   try {
-    return send(await handler(request), response)
+    const pathname = new URL(request.url).pathname
+    const moduleLoader = routes.get(pathname)
+
+    if (moduleLoader === undefined) {
+      return new Response(null, {
+        status: 404
+      })
+    }
+
+    const module = await moduleLoader()
+
+    const handler = /** @type {Handler | undefined} */ (module[request.method])
+
+    if (typeof handler !== 'function') {
+      const allowedMethods = Object.entries(module)
+        .filter(([, exportedValue]) => {
+          return typeof exportedValue === 'function'
+        })
+        .map(([method]) => method)
+
+      return new Response(null, {
+        status: 405,
+        headers: {
+          Allow: allowedMethods.join(', ')
+        }
+      })
+    }
+
+    return await handler(request)
   } catch (error) {
     console.error(error)
-    response.statusCode = 500
-    response.end()
+
+    return new Response(null, {
+      status: 500
+    })
   }
-}).listen(port, host, () => console.log(`http://${host}:${port}`))
+}
+
+const app = connect()
+
+app.use(
+  '/static',
+  serveStatic(join(import.meta.dirname, 'client'), {
+    index: false
+  })
+)
+
+app.use(toNodeHandler(fetchHandler))
+
+createServer(app).listen(port, host, () => console.log(`http://${host}:${port}`))
