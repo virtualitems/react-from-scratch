bcea6c04bd0cb4c461826a664bca378ac1d9697b
Alejandro CR <contacto@alejandrocr.co>
2026-07-18 03:04:50 -0500

    add baseic ssr example

---
+++ examples/01-basic/server.mjs
@@ -0,0 +1,41 @@
+import { createServer } from 'node:http'
+import { Readable } from 'node:stream'
+
+import { renderToReadableStream } from 'react-dom/server'
+
+import Document from '../shared/server/Document.mjs'
+import { createElement, h1 } from '../shared/hyperscript.mjs'
+
+function App() {
+  return createElement(
+    Document,
+    { title: 'dom-server' },
+    h1(null, 'Greetings!'),
+  )
+}
+
+/**
+ * @param {import('node:http').IncomingMessage} request
+ * @param {import('node:http').ServerResponse} response
+ */
+async function listener(request, response) {
+  console.info(new Date().toISOString(), request.method, request.url)
+
+  try {
+    const stream = await renderToReadableStream(App())
+    response.writeHead(200, { 'Content-Type': 'text/html' })
+    Readable.fromWeb(stream).pipe(response)
+  } catch (error) {
+    console.error(error)
+
+    if (response.headersSent === false) {
+      response.writeHead(500, {
+        'Content-Type': 'text/plain; charset=utf-8'
+      })
+    }
+
+    response.end('Internal Server Error')
+  }
+}
+
+createServer(listener).listen(3000, () => console.log('http://localhost:3000'))
